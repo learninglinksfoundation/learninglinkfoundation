fda788e9e301e72bd79a5645cc37a7e00849921c
nitin.shyamnani@cloudcertitude.com <=>
2021-11-18 19:25:50 +0530

    ''

--- routes/users.js
+++ routes/users.js
@@ -308,15 +308,16 @@ router.get('/getProjectMemeber',verify, (request, response) => {
     contactQueryResult.rows.forEach(dt=>{
         teamList.push(`'${dt.team__c}'`);
     });
-    pool.query(`SELECT Team__c, Representative__c, sfId, Name FROM salesforce.Team_Member__c WHERE Team__c IN ('${teamList.join(',')}') ORDER BY Name`)
+    console.log('1');
+    pool.query(`SELECT Team__c, Representative__c, sfId, Name FROM salesforce.Team_Member__c WHERE Team__c IN (${teamList.join(',')}) ORDER BY Name`)
       .then(data=>{
+        console.log('2');
         let conId = [];
-        console.log(`SELECT Team__c, Representative__c, sfId, Name FROM salesforce.Team_Member__c WHERE Team__c IN ('${teamList.join(',')}') ORDER BY Name`)
         data.rows.forEach(dt=>{
           conId.push( `'${dt.representative__c}'`);
         });
-        console.log(conId,`SELECT sfid, Name FROM salesforce.Contact WHERE sfid IN (${conId.join(',')}) ORDER BY Name`);
-        pool.query(`SELECT sfid, Name FROM salesforce.Contact WHERE sfid IN ('${conId.join(',')}') ORDER BY Name`)
+        console.log(conId);
+        pool.query(`SELECT sfid, Name FROM salesforce.Contact WHERE sfid IN (${conId.join(',')}) ORDER BY Name`)
         .then(data1=>{
           console.log(data1.rows);
           response.send(data1.rows);
