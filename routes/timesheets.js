c4be16a1c00bc14c4d502b7a8b4669d83f494d3d
nitin.shyamnani@cloudcertitude.com <=>
2021-11-24 16:17:01 +0530

    make it

--- routes/timesheets.js
+++ routes/timesheets.js
@@ -1497,7 +1497,7 @@ router.get('/getTeamdetails',verify,async(request,response)=>{
 router.get('/getTasklist',verify,(request,response)=>{
   let date = request.query.date;
   let objUser=request.user;
-  let queryText = 'SELECT tsk.Id,tsk.sfid as sfids,tsk.name as tskname,tsk.Task_Stage__c as stage,tsk.start_date__c ,tsk.Project_Name__c,tsk.Total_Hours__c ,tsk.assigned_manager__c,tsk.end_time__c,tsk.Task_Type__c,tsk.Planned_Hours__c,tsk.Start_Time__c,cont.sfid as contid ,cont.name as contname,proj.name as projname,tsk.createddate '+
+  let queryText = 'SELECT tsk.Id,tsk.sfid as sfids,tsk.name as tskname,tsk.Task_Stage__c as stage,tsk.start_date__c ,tsk.Project_Name__c,tsk.Total_Hours__c ,tsk.assigned_manager__c,tsk.end_time__c,tsk.Task_Type__c,tsk.Planned_Hours__c,tsk.Start_Time__c,cont.sfid as contid ,cont.name as contname,proj.sfid as projId,proj.name as projname,tsk.createddate '+
                    'FROM salesforce.Milestone1_Task__c tsk '+ 
                    'INNER JOIN salesforce.Contact cont ON tsk.assigned_manager__c = cont.sfid '+
                    'INNER JOIN salesforce.Milestone1_Project__c proj ON tsk.Project_Name__c= proj.sfid '+
@@ -1528,7 +1528,8 @@ router.get('/getTasklist',verify,(request,response)=>{
             createdDate.setMinutes(planDate.getMinutes() + 30);
             let strplanDate = planDate.toLocaleString();
 
-
+          obj.userId = eachRecord.contid;
+          obj.proId = eachRecord.projId;
           obj.function = eachRecord.function;
           obj.status = eachRecord.stage;
           obj.taskName = eachRecord.tskname;
