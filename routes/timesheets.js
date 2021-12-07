35b7f26b92b1f65f1db037efa0b91a0ae61c4228
nitin.shyamnani@cloudcertitude.com <=>
2021-12-07 19:11:01 +0530

    ''

--- routes/timesheets.js
+++ routes/timesheets.js
@@ -1615,7 +1615,7 @@ router.get('/getTasklist',verify,(request,response)=>{
   let queryText = 'SELECT tsk.Id,tsk.sfid as sfids, usr.sfid as createdById, usr.Name as createdName,tsk.name as tskname,tsk.Task_Stage__c as stage,tsk.start_date__c ,tsk.Project_Name__c,tsk.Total_Hours__c ,tsk.assigned_manager__c,tsk.end_time__c,tsk.Task_Type__c,tsk.Planned_Hours__c,tsk.Start_Time__c,cont.sfid as contid ,cont.name as contname,proj.name as projname,tsk.createddate '+
                    'FROM salesforce.Milestone1_Task__c tsk '+ 
                    'INNER JOIN salesforce.Contact cont ON tsk.assigned_manager__c = cont.sfid '+
-                   'INNER JOIN salesforce.User usr ON tsk.CreatedById = usr.sfid  '
+                   'INNER JOIN salesforce.User usr ON tsk.CreatedById = usr.sfid  ' +
                    'INNER JOIN salesforce.Milestone1_Project__c proj ON tsk.Project_Name__c= proj.sfid '+
                    'WHERE  tsk.Assigned_Manager__c= $1 AND tsk.sfid IS NOT NULL ';
                    
