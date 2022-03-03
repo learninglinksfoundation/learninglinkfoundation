3426392f135fd8ac20833f0cdbef0be8e358c801
nitin.shyamnani@cloudcertitude.com <=>
2022-03-03 10:46:14 +0530

    ''

--- routes/procurement.js
+++ routes/procurement.js
@@ -2060,7 +2060,7 @@ router.get('/getProcurementDetail',async(request,response)=>{
     let procurementId=request.query.procurementId;
     console.log('getProcurementITDetail Id='+procurementId);
     let procDetail={};
-    let qry='SELECT proc.sfid,proc.Name as procName ,proc.Geographic_Zone__c,proc.District_Upper_Zone__c,proc.Districts_Lower_Zone__c,asst.name as astname ,proc.Catgeory__c,proc.Total_Approved_Budget_for_the_purchase__c as purchasebudg  ,proc.Asset_Requisition_Form__c,proc.Items__c ,proc.Others__c,proc.Products_Services_Name__c,vend.name as vendorName,proc.Product_Service__c,proc.Quantity__c, proc.Budget__c,proc.Impaneled_Vendor__c, '+
+    let qry='SELECT proc.sfid,proc.Name as procName ,proc.Geographic_Zone__c,proc.District_Upper_Zone__c,proc.Districts_Lower_Zone__c,asst.name as astname ,asst.Accounts_Approval__c as accountapproval,proc.Catgeory__c,proc.Total_Approved_Budget_for_the_purchase__c as purchasebudg  ,proc.Asset_Requisition_Form__c,proc.Items__c ,proc.Others__c,proc.Products_Services_Name__c,vend.name as vendorName,proc.Product_Service__c,proc.Quantity__c, proc.Budget__c,proc.Impaneled_Vendor__c, '+
     'proc.State__c,proc.District__c,proc.Quote1__c,proc.Quote2__c,proc.Quote3__c,proc.Per_Unit_Cost__c,proc.unit__c,proc.Number_of_quotes__c,proc.justification__c '+
     'FROM salesforce.Product_Line_Item__c proc '+
     'INNER JOIN salesforce.Impaneled_Vendor__c vend '+
