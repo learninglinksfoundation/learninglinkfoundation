909914010da694f049d226c3a2ef4ecb8ba686fc
nitin.shyamnani@cloudcertitude.com <=>
2022-05-16 11:39:24 +0530

    ''

--- routes/timesheets.js
+++ routes/timesheets.js
@@ -1564,7 +1564,7 @@ router.get('/getTeamsProject',verify,async(request, response) => {
                        'FROM salesforce.Milestone1_Task__c tsk '+ 
                        'INNER JOIN salesforce.Contact cont ON tsk.assigned_manager__c = cont.sfid '+
                        'INNER JOIN salesforce.Milestone1_Project__c proj ON tsk.Project_Name__c= proj.sfid '+
-                       `WHERE tsk.sfid IS NOT NULL AND tsk.Assigned_Manager__c IN  (SELECT  Representative__c FROM salesforce.Team_Member__c WHERE team__c IN (${projTeampram.join(',')}) ) OR tsk.Task_Assigned_by__c = '${userId}'  `; 
+                       `WHERE tsk.sfid IS NOT NULL AND tsk.Assigned_Manager__c IN  (SELECT  Representative__c FROM salesforce.Team_Member__c WHERE team__c IN (${projTeampram.join(',')}) )  `; 
     console.log(queryText) ;
     if(proId){
       queryText = queryText + ` AND tsk.Project_Name__c = '${proId}'  `;
