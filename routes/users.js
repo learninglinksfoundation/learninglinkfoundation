25cbb54c2b7a74cf3df3108fb8418fc136fc535e
nitin.shyamnani@cloudcertitude.com <=>
2022-05-31 13:31:27 +0530

    ''

--- routes/users.js
+++ routes/users.js
@@ -2462,7 +2462,7 @@ router.get('/editProfile',verify,(request,response)=>{
   let objUser=request.user;
   let userId=objUser.sfid;
   console.log('Sfidddd :'+JSON.stringify(objUser));
-  let queryContact = 'SELECT sfid,email, Employee_ID__c, PM_email__c, Employee_Category_Band__c, address__c,MobilePhone, name FROM salesforce.contact where sfid=$1' ;
+  let queryContact = 'SELECT c.sfid,c.email, c.Employee_ID__c,c.reporting_manager__c ,c.PM_email__c, c.Employee_Category_Band__c, c.address__c,c.MobilePhone, c.name,con.name as reportingname FROM salesforce.contact INNER join salesforce.contact con on con.sfid = c.reporting_manager__c where sfid=$1  ' ;
   pool
   .query(queryContact,[userId])
   .then((queryResult)=>{
