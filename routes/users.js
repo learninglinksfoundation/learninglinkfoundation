db7ae5155560aa5b172d5a8334ee4c250b8f0220
Kamran <kamran.cloudcertitude@gmail.com>
2022-06-01 15:30:43 +0530

    m

--- routes/users.js
+++ routes/users.js
@@ -2479,7 +2479,7 @@ router.get('/editProfile',verify,(request,response)=>{
     console.log('user '+user);
     response.render('editProfile',{userI}); */
     response.render('editProfile',{userdetail, objUser});
-    response.redirect('/users/login');
+    response.render('login',{userdetail, objUser});
   })
   .catch((QueryError)=>{
     console.log('QueryError'+QueryError.stack);
