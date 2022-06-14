beaf0dd00e38fe5036f14f385564d9c753e78249
Kamran <kamran.cloudcertitude@gmail.com>
2022-06-14 14:46:48 +0530

    m

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ const {pool} = require('../db/dbConfig');
 const verify = require('../config/verifyToken');
 const jwt = require('jsonwebtoken');
 const joi = require('joi');
+const { joiPassword } = require('joi-password');
 const { response } = require('express');
 const { Client } = require('pg');
 // const {check, validationResult }=require('express-validator');
@@ -2450,7 +2451,7 @@ router.post('/updatePass',(request,response)=>{
   const { pass,pass2,user}=request.body;
 
    const schema = joi.object({
-    password:joi.string().required().label('Please Fill Password'),
+    password:joiPassword.string().required().label('Please Fill Password'),
     pass:joi.string().min(10).minOfNumeric(2).required().label('Password must be 10 characters long'),
     password2:joi.string().required().label('Please Re-enter Password'),  
     confirmPassword:joi.string().required().valid(joi.ref('password')).label('Passwords does not match'),
