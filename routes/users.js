be6a72f1f88200ec15bdced72d3d45d1dddb6a04
Kamran <kamran.cloudcertitude@gmail.com>
2022-06-15 15:41:56 +0530

    m

--- routes/users.js
+++ routes/users.js
@@ -2490,14 +2490,14 @@ router.get('/editProfile',verify,(request,response)=>{
   let objUser=request.user;
   let userId=objUser.sfid;
   console.log('Sfidddd :'+JSON.stringify(objUser));
-  let queryContact = 'SELECT c.sfid, c.profile_picture_url__c, c.email, c.employee_id__c,c.reporting_manager__c , c.pm_email__c, c.employee_category_band__c, c.address__c,c.mobilephone, c.name FROM salesforce.contact c  where c.sfid=$1  ' ;
+  let queryContact = 'SELECT c.sfid, c.profile_picture_url__c, c.email, c.employee_id__c,c.salesforce_reporting_manager__c , c.pm_email__c, c.employee_category_band__c, c.address__c,c.mobilephone, c.name FROM salesforce.contact c  where c.sfid=$1  ' ;
   pool
   .query(queryContact,[userId])
   .then( async (queryResult)=>{
 
     let userdetail=queryResult.rows[0];
     objUser.name = userdetail.name;
-    let resp = await pool.query('Select sfid,name from salesforce.contact where sfid = $1',[userdetail.reporting_manager__c])
+    let resp = await pool.query('Select sfid,name from salesforce.contact where sfid = $1',[userdetail.salesforce_reporting_manager__c])
     console.log('userdeat '+JSON.stringify(userdetail));
     userdetail.reportingname = resp.rows.length > 0 ? resp.rows[0].name : ''
  /*    console.log('queryResult'+JSON.stringify(queryResult.rows));
