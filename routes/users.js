ea24cd83d403914ff99b40d15e2f6731fdf24798
Kamran <kamran.cloudcertitude@gmail.com>
2022-06-17 16:00:05 +0530

    m

--- routes/users.js
+++ routes/users.js
@@ -2453,7 +2453,7 @@ router.post('/updatePass',(request,response)=>{
  // let pattern = '/^(?=.*[A-Z]).*$' ;
   const schema = joi.object({
     password:joi.string().required().label('Please Fill Password'),
-    pass:joi.string().regex( /^(?=.*[0-9])/).required().label('minimum char 10'),
+    pass:joi.string().regex( /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,15}$/).required().label('minimum char 10'),
     password2:joi.string().required().label('Please Re-enter Password'),  
     confirmPassword:joi.string().required().valid(joi.ref('password')).label('Passwords does not match'),
       })
