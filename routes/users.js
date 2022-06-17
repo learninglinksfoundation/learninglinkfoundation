5e9d436170653e75bc0fac68a4f2ea36e9f956c4
Kamran <kamran.cloudcertitude@gmail.com>
2022-06-17 17:07:22 +0530

    m

--- routes/users.js
+++ routes/users.js
@@ -2449,11 +2449,10 @@ router.get('/generatePassword/:userId',(request,response)=>{
 router.post('/updatePass',(request,response)=>{
   console.log('BODy'+JSON.stringify(request.body));
   const { pass,pass2,user}=request.body;
-  //const pattern = "/(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[$@$!#.])[A-Za-zd$@$!%*?&.]{8,20}/";
- // let pattern = '/^(?=.*[A-Z]).*$' ;
+ 
   const schema = joi.object({
     password:joi.string().required().label('Please Fill Password'),
-    pass:joi.string().regex( /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,15}$/).required().label('Password must contain 1 digit, 1 uppercase, 1 lowercase and 1 specialcharacter'),
+    pass:joi.string().regex( /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,15}$/).required().label('Password must contain 10 digits 1 number, 1 uppercase, 1 lowercase and 1 specialcharacter'),
     password2:joi.string().required().label('Please Re-enter Password'),  
     confirmPassword:joi.string().required().valid(joi.ref('password')).label('Passwords does not match'),
       })
