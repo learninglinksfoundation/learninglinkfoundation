5ff97955617da891526341a7f401437cf5645acd
Kamran <kamran.cloudcertitude@gmail.com>
2022-06-29 15:49:30 +0530

    m

--- routes/users.js
+++ routes/users.js
@@ -2496,7 +2496,7 @@ router.post('/updatePass',(request,response)=>{
  
   const schema = joi.object({
     password:joi.string().required().label('Please Fill Password'),
-    pass:joi.string().regex( /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,15}$/).required().label('Password must contain 10 digits 1 number, 1 uppercase, 1 lowercase and 1 specialcharacter'),
+    pass:joi.string().regex( /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,15}$/).required().label('Password must contain atleast 10 characters, 1 upper case, 1 lower case & 1 special character (&,%,!,@ ...)'),
     password2:joi.string().required().label('Please Re-enter Password'),  
     confirmPassword:joi.string().required().valid(joi.ref('password')).label('Passwords does not match'),
       })
